500f5cc289d79f7e4af4b94aecd6a927c9351906
William Kennedy <bill@ardanlabs.com>
2022-07-29 11:04:46 +0200

    commit2

--- app/services/sales-api/main.go
+++ app/services/sales-api/main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/signal"
+	"runtime"
+	"syscall"
 
 	"github.com/ardanlabs/service/foundation/logger"
+	"go.uber.org/automaxprocs/maxprocs"
 	"go.uber.org/zap"
 )
 
@@ -27,6 +31,26 @@ func main() {
 }
 
 func run(log *zap.SugaredLogger) error {
+
+	// =========================================================================
+	// GOMAXPROCS
+
+	// Want to see what maxprocs reports.
+	opt := maxprocs.Logger(log.Infof)
+
+	// Set the correct number of threads for the service
+	// based on what is available either by the machine or quotas.
+	if _, err := maxprocs.Set(opt); err != nil {
+		return fmt.Errorf("maxprocs: %w", err)
+	}
+	log.Infow("startup", "GOMAXPROCS", runtime.GOMAXPROCS(0))
+
+	// Just a hack for now.
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+	<-shutdown
+	log.Infow("shutdown started")
+	defer log.Infow("shutdown completed")
 
 	return nil
 }
