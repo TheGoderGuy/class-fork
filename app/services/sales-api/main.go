df9c96ff3c709f6fa26d4186d552b1a8ccaa354c
William Kennedy <bill@ardanlabs.com>
2022-07-29 11:25:03 +0200

    commit3

--- app/services/sales-api/main.go
+++ app/services/sales-api/main.go
@@ -8,7 +8,6 @@ import (
 	"syscall"
 
 	"github.com/ardanlabs/service/foundation/logger"
-	"go.uber.org/automaxprocs/maxprocs"
 	"go.uber.org/zap"
 )
 
@@ -36,13 +35,13 @@ func run(log *zap.SugaredLogger) error {
 	// GOMAXPROCS
 
 	// Want to see what maxprocs reports.
-	opt := maxprocs.Logger(log.Infof)
+	// opt := maxprocs.Logger(log.Infof)
 
 	// Set the correct number of threads for the service
 	// based on what is available either by the machine or quotas.
-	if _, err := maxprocs.Set(opt); err != nil {
-		return fmt.Errorf("maxprocs: %w", err)
-	}
+	// if _, err := maxprocs.Set(opt); err != nil {
+	// 	return fmt.Errorf("maxprocs: %w", err)
+	// }
 	log.Infow("startup", "GOMAXPROCS", runtime.GOMAXPROCS(0))
 
 	// Just a hack for now.
