54d25f5f3db92f8883ed7635dee88bd59ef41df6
William Kennedy <bill@ardanlabs.com>
2022-07-29 11:02:05 +0200

    commit 1

---
+++ app/services/sales-api/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/ardanlabs/service/foundation/logger"
+	"go.uber.org/zap"
+)
+
+func main() {
+
+	// Construct the application logger.
+	log, err := logger.New("SALES-API")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer log.Sync()
+
+	// Perform the startup and shutdown sequence.
+	if err := run(log); err != nil {
+		log.Errorw("startup", "ERROR", err)
+		log.Sync()
+		os.Exit(1)
+	}
+}
+
+func run(log *zap.SugaredLogger) error {
+
+	return nil
+}
